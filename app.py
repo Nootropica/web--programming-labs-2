8c0cc75d4136ff77ca9b3cf795eb9dbdfb052d39
Владислав Печенкин <mogucci2000@list.ru>
2024-09-27 16:37:45 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Печенкин Владислав Витальевич, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Владислав Печенкин, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
