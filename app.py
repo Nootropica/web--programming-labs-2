d2037ce001bd7c687e5705e255f850b5e7851597
Владислав Печенкин <mogucci2000@list.ru>
2024-10-09 22:55:41 +0700

    Имя студента

--- app.py
+++ app.py
@@ -211,4 +211,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-     return render_template('example.html')
+     name = 'Владислав Кувалда'
+     return render_template('example.html', name=name)
