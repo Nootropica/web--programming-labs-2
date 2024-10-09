9b4a5ab36cd88e2e8eb8923aa360f37f7835be93
Владислав Печенкин <mogucci2000@list.ru>
2024-10-09 20:29:08 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -182,5 +182,14 @@ def a():
      return 'без слэша'
 
 @app.route('/lab2/a/')
-def a():
+def a2():
     return 'со слэшем'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        return "такого цветка нет", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
