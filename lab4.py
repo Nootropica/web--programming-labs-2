7ba5511d6415d994959895116eeaffbf0b7a0319
Владислав Печенкин <mogucci2000@list.ru>
2024-11-08 23:50:42 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -172,3 +172,39 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    if 'login' not in session:
+        return redirect('/lab4/login')
+
+    if request.method == 'POST':
+        if 'reset' in request.form:
+            session.pop('fridge_temperature', None)
+        else:
+            temperature = request.form.get('temperature')
+            if temperature:
+                temperature = int(temperature)
+                session['fridge_temperature'] = temperature
+
+    temperature = session.get('fridge_temperature', None)
+    message = ''
+    snowflakes = 0
+
+    if temperature is None:
+        message = 'Ошибка: не задана температура'
+    elif temperature < -12:
+        message = 'Не удалось установить температуру — слишком низкое значение'
+    elif temperature > -1:
+        message = 'Не удалось установить температуру — слишком высокое значение'
+    elif -12 <= temperature <= -9:
+        message = f'Установлена температура: {temperature}°С'
+        snowflakes = 3
+    elif -8 <= temperature <= -5:
+        message = f'Установлена температура: {temperature}°С'
+        snowflakes = 2
+    elif -4 <= temperature <= -1:
+        message = f'Установлена температура: {temperature}°С'
+        snowflakes = 1
+
+    return render_template('lab4/fridge.html', message=message, snowflakes=snowflakes)
