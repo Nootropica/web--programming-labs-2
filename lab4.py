1bf86de742cb14148ba5077c9709665d8e0887d8
Владислав Печенкин <mogucci2000@list.ru>
2024-11-08 21:49:49 +0700

    Счетчик деревьев

--- lab4.py
+++ lab4.py
@@ -26,3 +26,103 @@ def div():
     
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1 = request.form.get('x1', '')
+    x2 = request.form.get('x2', '')
+    
+    if x1 == '':
+        x1 = 0
+    else:
+        x1 = int(x1)
+    
+    if x2 == '':
+        x2 = 0
+    else:
+        x2 = int(x2)
+    
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result, back_link='/lab4/sum-form')
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1', 1)
+    x2 = request.form.get('x2', 1)
+    
+    if x1 == '':
+        x1 = 1
+    else:
+        x1 = int(x1)
+    
+    if x2 == '':
+        x2 = 1
+    else:
+        x2 = int(x2)
+    
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result, back_link='/lab4/mul-form')
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!', back_link='/lab4/sub-form')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result, back_link='/lab4/sub-form')
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!', back_link='/lab4/pow-form')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/pow.html', error='Оба поля не могут быть нулями!', back_link='/lab4/pow-form')
+    
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result, back_link='/lab4/pow-form')
+
+tree_count = 0
+
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+
+    operation = request.form.get('operation')
+
+    if operation == 'cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count += 1
+
+    return render_template('lab4/tree.html', tree_count=tree_count)
