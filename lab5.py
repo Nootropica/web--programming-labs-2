dc9a92c6476d1e4caf5dcf36b5bc2f8ee0cbeef3
Владислав Печенкин <mogucci2000@list.ru>
2024-11-22 23:28:33 +0700

    Авторизация

--- lab5.py
+++ lab5.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab5 = Blueprint('lab5', __name__)
 
@@ -8,9 +9,42 @@ def index():
     username = session.get('username', 'anonymous')
     return render_template('lab5/menu.html', username=username)
 
-@lab5.route('/lab5/login')
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
 def login():
-    return "Страница входа"
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='vladislav_pechenkin_knowledge_base',
+        user='vladislav_pechenkin_knowledge_base',
+        password='123'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    cur.close()
+    conn.close()
+    session['username'] = login
+    return render_template('lab5/success_login.html', login=login)
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -24,10 +58,10 @@ def register():
         return render_template('lab5/register.html', error='Заполните все поля')
     
     conn = psycopg2.connect(
-        host ='127.0.0.1',
-        database ='vladislav_pechenkin_knowledge_base',
-        user ='vladislav_pechenkin_knowledge_base',
-        password = '123'
+        host='127.0.0.1',
+        database='vladislav_pechenkin_knowledge_base',
+        user='vladislav_pechenkin_knowledge_base',
+        password='123'
     )
     cur = conn.cursor()
 
@@ -35,8 +69,7 @@ def register():
     if cur.fetchone():
         cur.close()
         conn.close()
-        return render_template('lab5/register.html',
-                               error="Такой пользователь уже существует")
+        return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
     conn.commit()
