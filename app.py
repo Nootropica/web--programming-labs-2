5e70b74f66a5c4adfe558605768d441932c02928
Владислав Печенкин <mogucci2000@list.ru>
2024-12-21 05:48:06 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from flask import Flask
+import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from flask import Flask, redirect, url_for, render_template, session
 from lab1 import lab1
 from lab2 import lab2
@@ -10,7 +16,22 @@ from lab8 import lab8
 
 app = Flask(__name__)
 
-app.secret_key = 'секретно-секретный секрет'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'vladislav_pechenkin_orm'
+    db_user = 'vladislav_pechenkin_orm'
+    db_password = ' 123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app. config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://(db_user):{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "vladislav_pechenkin_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path} '
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
