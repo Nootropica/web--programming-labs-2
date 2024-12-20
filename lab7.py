bb7eabb555047ead3dd17499320bf8e6fc815473
Владислав Печенкин <mogucci2000@list.ru>
2024-12-21 06:24:38 +0700

    Авторизация

---
+++ lab7.py
@@ -0,0 +1,140 @@
+from flask import Flask, Blueprint, render_template, request, jsonify
+from datetime import datetime
+import psycopg2
+
+app = Flask(__name__)
+
+DB_CONFIG = {
+    'dbname': 'vladislav_pechenkin_knowledge_base',  
+    'user': 'vladislav_pechenkin_knowledge_base',
+    'password': '123',  
+    'host': '127.0.0.1',  
+}
+
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        print("Успешное подключение к базе данных")  
+        return conn
+    except Exception as e:
+        print("Ошибка подключения к базе данных:", e)
+        return None
+
+lab7 = Blueprint('lab7', __name__)
+
+@lab7.route('/lab7/')
+def main():
+    return render_template('lab7/index.html')
+
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM films')
+    films = cursor.fetchall()
+    conn.close()
+    return jsonify([{'id': film[0], 'title': film[1], 'title_ru': film[2], 'year': film[3], 'description': film[4]} for film in films])
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM films WHERE id = %s', (id,))
+    film = cursor.fetchone()
+    conn.close()
+    if film is None:
+        return jsonify({"error": "Film not found"}), 404
+    return jsonify({'id': film[0], 'title': film[1], 'title_ru': film[2], 'year': film[3], 'description': film[4]})
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
+
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM films WHERE id = %s', (id,))
+    conn.commit()
+    conn.close()
+    return '', 204
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
+
+    cursor = conn.cursor()
+    film = request.get_json()
+
+ 
+    if not film.get('title_ru'):
+        return jsonify({"error": "Русское название не может быть пустым"}), 400
+
+    if not film.get('title') and not film.get('title_ru'):
+        return jsonify({"error": "Оригинальное название не может быть пустым, если русское название пустое"}), 400
+
+    if not film.get('year') or not (1895 <= int(film['year']) <= datetime.now().year):
+        return jsonify({"error": "Год должен быть от 1895 до текущего года"}), 400
+
+    if not film.get('description') or len(film['description']) > 2000:
+        return jsonify({"error": "Описание должно быть непустым и не более 2000 символов"}), 400
+
+  
+    if not film.get('title'):
+        film['title'] = film['title_ru']
+
+    cursor.execute('''
+        UPDATE films
+        SET title = %s, title_ru = %s, year = %s, description = %s
+        WHERE id = %s
+    ''', (film['title'], film['title_ru'], film['year'], film['description'], id))
+    conn.commit()
+    conn.close()
+    return jsonify(film)
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film():
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
+
+    cursor = conn.cursor()
+    new_film = request.get_json()
+
+
+    if not new_film.get('title_ru'):
+        return jsonify({"error": "Русское название не может быть пустым"}), 400
+
+    if not new_film.get('title') and not new_film.get('title_ru'):
+        return jsonify({"error": "Оригинальное название не может быть пустым, если русское название пустое"}), 400
+
+    if not new_film.get('year') or not (1895 <= int(new_film['year']) <= datetime.now().year):
+        return jsonify({"error": "Год должен быть от 1895 до текущего года"}), 400
+
+    if not new_film.get('description') or len(new_film['description']) > 2000:
+        return jsonify({"error": "Описание должно быть непустым и не более 2000 символов"}), 400
+
+    if not new_film.get('title'):
+        new_film['title'] = new_film['title_ru']
+
+    cursor.execute('''
+        INSERT INTO films (title, title_ru, year, description)
+        VALUES (%s, %s, %s, %s)
+        RETURNING id
+    ''', (new_film['title'], new_film['title_ru'], new_film['year'], new_film['description']))
+    new_film_id = cursor.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Film added", "id": new_film_id}), 201
+
+app.register_blueprint(lab7)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- lab8.py
+++ lab8.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 from werkzeug.security import generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
@@ -12,7 +13,21 @@ def index():
 
 @lab8.route('/lab8/login', methods=['GET', 'POST'])
 def login():
-    return render_template('lab8/login.html')
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login_form')
+    password_form = request.form.get('password')
+
+    user = users.query.filter_by(login_form).firts()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember=False)
+            return redirect('/lab8/')
+        
+    return render_template('/lab8/login.html',
+                           error = 'Ошибка входа: логин и/или пароль неверны') 
 
 @lab8.route('/lab8/register', methods=['GET', 'POST'])
 def register():
@@ -41,6 +56,7 @@ def register():
 def create():
     return render_template('lab8/create.html')
     
-@lab8.route('/lab8/articles', methods=['GET', 'POST'])
-def articles():
-    return render_template('lab8/articles.html')
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "список статей"
